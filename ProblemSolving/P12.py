a7c6782918f5e50f20c22bd21e0f2155a62bb73c
ankit9949208 <ankit.jagdish@elucidata.io>
2021-05-28 16:03:46 +0530

    Odd even subsequences

--- ProblemSolving/P12.py
+++ ProblemSolving/P12.py
@@ -5,9 +5,12 @@ class OddEvenSubsequences:
   def maxLength(self, A):
     toggle = "odd" if A[0]%2==0 else "even"
     B=[A[0]]
+    slowPointer=1
     for i in range(len(A))[1:]:
       if (A[i]%2!=0 and toggle == "odd") or(A[i]%2==0 and toggle == "even"):
         B.append(A[i])
+        A[slowPointer]=A[i]
+        slowPointer+=1
         toggle= "odd" if toggle=="even" else "even"
     return B
 
